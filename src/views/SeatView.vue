75f23e91901a2142416d8071d4aa515460f0215b
Kejie <1728635615@qq.com>
2023-12-28 15:10:08 +0800

    实现购票

--- src/views/SeatView.vue
+++ src/views/SeatView.vue
@@ -6,11 +6,13 @@ import IconSeat from "../icon/IconSeat.vue"
     <div>
         <div v-for="(item, i) in schedule.seatInfo" :key="item" class="SeatBox">
             第-{{ i }}-排
-            <div v-for="index in item" :key="index">
-                <IconSeat v-if="index == 0" :color="iconColor" :size="iconSize"></IconSeat>
-                <IconSeat v-else-if="index == 1" :color="iconColorBlue" :size="iconSize"></IconSeat>
+            <div v-for="(index, j) in item" :key="index">
+                <IconSeat v-if="index == 0" :color="getColor(i, j)" :size="iconSize" @click="changeColor(i, j)"></IconSeat>
+                <IconSeat v-else-if="index == 1" :color="getColor(i, j)" :size="iconSize" @click="changeColor(i, j)">
+                </IconSeat>
             </div>
         </div>
+        <div @click="changeSeatInfo()">确定购票</div>
     </div>
 </template>
 
@@ -43,9 +45,8 @@ export default {
     data() {
         return {
             schedule: {},
-            iconColor: 'gray', // 定义图标颜色，默认为红色
-            iconColorBlue: 'blue', // 定义图标颜色，默认为红色
             iconSize: '24px', // 定义图标大小，默认为24像素
+            i: 0
         };
     },
     mounted() {
@@ -64,6 +65,34 @@ export default {
             })
             this.schedule.seatInfo = JSON.parse(this.schedule.seatInfo)
         },
+        getColor(rowIndex, colIndex) {
+            return this.schedule.seatInfo[rowIndex][colIndex] == 0 ? 'gray' : 'blue';
+        },
+        changeColor(rowIndex, colIndex) {
+            if (this.schedule.seatInfo[rowIndex][colIndex] == 1) {
+                this.schedule.seatInfo[rowIndex][colIndex] = 0
+                this.i--
+            } else {
+                if (this.i == 6) {
+                    alert('您已选择了六个，无法再选择')
+                    return
+                }
+                this.schedule.seatInfo[rowIndex][colIndex] = 1
+                this.i++
+            }
+        },
+        async changeSeatInfo() {
+            await axios({
+                method: 'post',
+                url: 'http://localhost:8000/cinema/changeScheduleInfo',
+                data: {
+                    hashID: this.$route.query.hashID,
+                    SeatInfo: JSON.stringify(this.schedule.seatInfo)
+                }
+            })
+            alert('购买成功')
+            window.location.reload()
+        }
     }
 };
 </script>
